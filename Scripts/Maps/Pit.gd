a56ade0a681faa6dd2111e4062011421b8eb956d
ShadowFungi <shadowfungi.meddr@aleeas.com>
2022-05-16 16:36:51 -0600

    change button texture

--- Scripts/Maps/Pit.gd
+++ Scripts/Maps/Pit.gd
@@ -3,6 +3,6 @@ extends Spatial
 
 
 func _on_entity_99_trigger_trigger():
-	get_tree().root.get_node("Control/GameOver").show()
+	get_tree().root.get_node("Node/GameOver").show()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().paused = true
