ec0ed5b426881c508a50fe7db0573ae85772de5a
ShadowFungi <shadowfungi.meddr@aleeas.com>
2022-06-12 17:00:48 -0600

    reworking primary map

--- Scripts/Maps/Pit.gd
+++ Scripts/Maps/Pit.gd
@@ -3,6 +3,6 @@ extends Spatial
 
 
 func _on_death_trigger():
-	get_tree().root.get_node("Node/GameOver").show()
+	get_tree().root.get_node("Node/ViewContainer/ViewportContainer/Viewport/Player/GameOver").show()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().paused = true
